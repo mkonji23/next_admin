297617fa3ac699f331c2122ece7b8570d3c081ce
yh.min <mkonji23@naver.com>
2025-03-26 17:23:05 +0900

    paging 연습완료

--- layout/AppMenu.tsx
+++ layout/AppMenu.tsx
@@ -13,7 +13,10 @@ const AppMenu = () => {
     const model: AppMenuItem[] = [
         {
             label: 'Home',
-            items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' }]
+            items: [
+                { label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' },
+                { label: 'Test', icon: 'pi pi-fw pi-circle-fill', to: '/test' }
+            ]
         },
         {
             label: 'UI Components',
