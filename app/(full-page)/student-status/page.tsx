10bc018fe82b18bfb4aad68055dd65af20563e23
yh.min <mkonji23@naver.com>
2026-05-26 14:44:42 +0900

    ㄱㄱ

--- app/(full-page)/student-status/page.tsx
+++ app/(full-page)/student-status/page.tsx
@@ -1,10 +1,17 @@
-'use client';
-
 import React from 'react';
+import { Metadata } from 'next';
 import StudentStatusContent from '@/components/studentStatus/StudentStatusContent';
 
-const StudentStatusPage = () => {
-    return <StudentStatusContent />;
+export const metadata: Metadata = {
+    title: '나의 칭찬 현황 보기',
+    description: '칭찬, 출석현황 그리고 평가메시지를 확인하세요!',
+    openGraph: {
+        title: '나의 칭찬 현황 보기',
+        description: '칭찬, 출석현황 그리고 평가메시지를 확인하세요!',
+        type: 'website'
+    }
 };
 
-export default StudentStatusPage;
+export default function StudentStatusPage() {
+    return <StudentStatusContent />;
+}
